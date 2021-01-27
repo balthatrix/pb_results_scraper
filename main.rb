444186d40dac858cef98b2005a714bd4fb968a8c
Jeron <jeron@JeronP.local>
2021-01-26 19:19:24 -0600

    results csv udpated

--- main.rb
+++ main.rb
@@ -1,10 +1,12 @@
 require "./lib/pb_result_parser"
 
 puts ARGV
+first_iteration = true
 ARGV.each do |pb_results_html|
   next if pb_results_html == "true"
   puts "All matches"
   pp PbResultsParser.results(File.open(pb_results_html))
   puts "==="
-  PbResultsParser.to_csv(File.open(pb_results_html), "results", ARGV.last == "true")
+  PbResultsParser.to_csv(File.open(pb_results_html), "results", ARGV.last == "true" || !first_iteration)
+  first_iteration = false
 end
